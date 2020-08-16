f3c7009bfc6a9685fafc269aa2a4815d06ff9031
Aidan Paetsch <aapaetsch@gmail.com>
2020-08-16 15:50:45 -0600

    Updates

---
+++ precache-manifest.a652962cd8c8315debf024657070552c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41f8e41dbf18e55aba4bb80093ca22d",
+    "url": "/stockViewer/index.html"
+  },
+  {
+    "revision": "32ec2b5282f408021e0b",
+    "url": "/stockViewer/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "4959f4292bf8e57f84d9",
+    "url": "/stockViewer/static/css/main.9f819746.chunk.css"
+  },
+  {
+    "revision": "32ec2b5282f408021e0b",
+    "url": "/stockViewer/static/js/2.07f089c5.chunk.js"
+  },
+  {
+    "revision": "4ad3e6f631465a54edafc99c1e22adbd",
+    "url": "/stockViewer/static/js/2.07f089c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4959f4292bf8e57f84d9",
+    "url": "/stockViewer/static/js/main.ab3ecd10.chunk.js"
+  },
+  {
+    "revision": "57c01101acff5138bc07",
+    "url": "/stockViewer/static/js/runtime-main.47f9175f.js"
+  }
+]);
